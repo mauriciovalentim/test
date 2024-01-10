4d5bbf758a0e44e74a809eed6d4a9c08ec4aff67
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2024-01-10 16:11:48 -0300

    getting pickle back

--- Model.py
+++ Model.py
@@ -2,13 +2,13 @@ import pickle
 from Scaler import Scaler
 import os
 
-# file_path = './saved_regressor.pkl'
+file_path = './saved_regressor.pkl'
 
-# if not os.path.exists(file_path):
-#     raise FileNotFoundError(f'O arquivo {file_path} não foi encontrado.')
-# else:
-#     with open(file_path, 'rb') as file:
-#         loaded_model, _, scaler_pickle = pickle.load(file)
+if not os.path.exists(file_path):
+    raise FileNotFoundError(f'O arquivo {file_path} não foi encontrado.')
+else:
+    with open(file_path, 'rb') as file:
+        loaded_model, _, scaler_pickle = pickle.load(file)
 
 scaler_instance = Scaler()
 
