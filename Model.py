988cd99abc8e714e3dc4c7b76915d7907c005788
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2024-01-11 15:38:37 -0300

    adding requirements

--- Model.py
+++ Model.py
@@ -13,14 +13,19 @@ else:
 scaler_instance = Scaler()
 
 def get_saida_dbo(*info):
+    print("-"*50)
     # Criar DataFrame a partir dos parâmetros
     original_data = scaler_instance.create_dataframe(info)
+    print(original_data.head())
     # Normalizando dataframe
     normalized_data = scaler_instance.normalize(original_data)
+    print(normalized_data)
     # Obtendo saída normalizada
-    # normalized_dbo = loaded_model.predict(normalized_data)[0]
-    normalized_dbo = 10
+    normalized_dbo = loaded_model.predict(normalized_data)[0]
+    print(normalized_dbo)
     # Desnormalizando saída
     desnormalized_dbo = scaler_instance.desnormalize(normalized_dbo)
+    print(desnormalized_dbo)
     # Retornando resultado (DBO SAÍDA DESNORMALIZADA)
+    print("-"*50)
     return desnormalized_dbo
