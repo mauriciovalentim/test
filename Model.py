b8d715ba9e0130e064cea70f741ab6beb84d0c62
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2024-01-11 16:01:12 -0300

    testing pickle

--- Model.py
+++ Model.py
@@ -2,13 +2,13 @@ import pickle
 from Scaler import Scaler
 import os
 
-file_path = './saved_regressor.pkl'
+# file_path = './saved_regressor.pkl'
 
-if not os.path.exists(file_path):
-    raise FileNotFoundError(f'O arquivo {file_path} não foi encontrado.')
-else:
-    with open(file_path, 'rb') as file:
-        loaded_model, _, scaler_pickle = pickle.load(file)
+# if not os.path.exists(file_path):
+#     raise FileNotFoundError(f'O arquivo {file_path} não foi encontrado.')
+# else:
+#     with open(file_path, 'rb') as file:
+#         loaded_model, _, scaler_pickle = pickle.load(file)
 
 scaler_instance = Scaler()
 
@@ -21,7 +21,8 @@ def get_saida_dbo(*info):
     normalized_data = scaler_instance.normalize(original_data)
     print(normalized_data)
     # Obtendo saída normalizada
-    normalized_dbo = loaded_model.predict(normalized_data)[0]
+    # normalized_dbo = loaded_model.predict(normalized_data)[0]
+    normalized_dbo = 4
     print(normalized_dbo)
     # Desnormalizando saída
     desnormalized_dbo = scaler_instance.desnormalize(normalized_dbo)
